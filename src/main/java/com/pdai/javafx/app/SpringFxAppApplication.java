ff643e585234b05795a3e33973f7f4c1531b42fe
daipeng <peng.dai@siemens.com>
2020-09-01 17:21:13 +0800

    'a'

--- src/main/java/com/pdai/javafx/app/SpringFxAppApplication.java
+++ src/main/java/com/pdai/javafx/app/SpringFxAppApplication.java
@@ -1,11 +1,10 @@
 package com.pdai.javafx.app;
 
-import java.util.Arrays;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import com.pdai.javafx.app.fx.AbstractFxApplication;
 import com.pdai.javafx.app.fx.FxmlView;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
 
 /**
  * <b>ClassName</b>: SpringFxAppApplication <br/>
@@ -13,18 +12,16 @@ import com.pdai.javafx.app.fx.FxmlView;
  * <b>Description</b>: SpringFxAppApplication <br/>
  *
  * <b>Date</b>: Apr 12, 2019 11:05:23 AM <br/>
- * 
+ *
  * @author pdai
  * @version Apr 12, 2019
- *
  */
 @SpringBootApplication
 public class SpringFxAppApplication extends AbstractFxApplication {
 
-	public static void main(String[] args) {
-		run(SpringFxAppApplication.class,
-				Arrays.asList(new FxmlView[] { FxmlView.MODULE_DASHBOARD, FxmlView.MODULE_PROFILE, FxmlView.MAIN }),
-				FxmlView.MAIN, args);
-	}
-
+    public static void main(String[] args) {
+        run(SpringFxAppApplication.class,
+                Arrays.asList(new FxmlView[]{FxmlView.MODULE_DASHBOARD, FxmlView.MODULE_PROFILE, FxmlView.MAIN}),
+                FxmlView.MAIN, args);
+    }
 }
--- src/test/java/com/pdai/javafx/app/SpringFxAppApplicationTests.java
+++ src/test/java/com/pdai/javafx/app/SpringFxAppApplicationTests.java
@@ -1,16 +1,16 @@
-package com.pdai.javafx.app;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class SpringFxAppApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
-
-}
+//package com.pdai.javafx.app;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class SpringFxAppApplicationTests {
+//
+//	@Test
+//	public void contextLoads() {
+//	}
+//
+//}
